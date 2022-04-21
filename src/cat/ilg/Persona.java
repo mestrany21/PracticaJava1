307641a1369514e0f7c17a0faddc65a7888f6b4c
mestrany21 <mestrany21@ilg.cat>
2022-04-21 18:35:43 +0200

    tercer commit, falta canviar a Date i 4

--- src/cat/ilg/Persona.java
+++ src/cat/ilg/Persona.java
@@ -15,15 +15,22 @@ public class Persona {
     public void assignarDni(String dni){
         if (this.dni == null){
             this.dni = dni;
+
         }else{
+
             System.out.println("ERROR");
         }
 
     }
 
     public String obtenirDades(){
+            if(this.dni==null || this.nom == null){
+                return "Dades incompletes";
+            }
+            else{
+                return "Persona que es diu " + this.nom + " amb DNI: " + this.dni;
+            }
 
-            return "Persona que es diu " + this.nom + " amb DNI: " + this.dni;
         }
     }
 
