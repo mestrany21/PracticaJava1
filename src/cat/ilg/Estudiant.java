20cbfa337fbdee5156df72c0706a7def986f702d
super <mestrany21@ilg.cat>
2022-05-06 18:54:27 +0200

    COMMIT final acabat

--- src/cat/ilg/Estudiant.java
+++ src/cat/ilg/Estudiant.java
@@ -6,7 +6,7 @@ public class Estudiant extends Persona  {
     private double nota;
 
     public Estudiant() {}
-    ArrayList notes = new ArrayList();
+    ArrayList<Double> notes = new ArrayList<>();
     public void posarNota(double nota) throws Exception{
         if(nota <= 10 && nota >=0) {
             notes.add(nota);
@@ -17,20 +17,40 @@ public class Estudiant extends Persona  {
     }
 
 
-    public String obtenirDades(){
-        if (super.obtenirDades() == "Dades incompletes"){
-        return "Dades incompletes";
-    }else {
-        return super.obtenirDades() + " (Estudiant)que te nota: " + this.notes;
-        }
-    }
-
     public double ferMitjana(){
 
         double mitjana = 0;
         double suma = 0;
-        for (int i=0; i<=notes.size(); i++){ //T'HAS QUEDAT AQUIIII!!!! FALAT MITJANA, NOTA MAX I NOTA MIN
+        for (int i=0; i<notes.size(); i++){
+            suma= suma + notes.get(i);
+        }
+        mitjana= suma/notes.size();
+        return mitjana;
+    }
 
+    public double obtenirNotaMin(){
+        double min = 10;
+        for (int i=0; i<notes.size(); i++){
+            if( min > notes.get(i)){
+                min = notes.get(i);
+            }
+        }
+        return min;
+    }
+    public double obtenirNotaMax(){
+        double max = 0;
+        for (int i=0; i<notes.size(); i++){
+            if( max < notes.get(i)){
+                max = notes.get(i);
+            }
+        }
+        return max;
+    }
+    public String obtenirDades(){
+        if (super.obtenirDades() == "Dades incompletes"){
+            return "Dades incompletes";
+        }else {
+            return super.obtenirDades() + " (Estudiant)que te nota: " + this.notes + " una mitjana de " + ferMitjana() + "una nota maxima de " + obtenirNotaMax() +" una nota mínima de " + obtenirNotaMin();
         }
     }
 }
